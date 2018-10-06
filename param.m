5be24f42b530443cc5f76011fe1eaae98ea064a0
Mikhail Rodkin <rodkinmm@yandex.ru>
2018-10-06 20:26:49 +0300

    initial commit

---
+++ param.m
@@ -0,0 +1,11 @@
+%% common
+dt = 0.0001;   % sampling
+
+%% signal
+freq = 1000;    % signal frequency
+phi0 = pi/4;    % initial signal phase
+ampl = 1;       % signal amplitude
+T = 0.15;       % signal duration
+
+%% noise
+noiseVariance = 0.5;
