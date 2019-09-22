f14af3f7078c0c58925dff473169b44f2e45caba
Mikhail Rodkin <rodkinmm@yandex.ru>
2019-09-22 21:34:49 +0300

    5KHz frequency & 20 KHz sampling

--- param.m
+++ param.m
@@ -1,14 +1,14 @@
 %% common
-dt = 0.00025;   % sampling = 4 samples per period
+dt = 0.00005;   % sampling = 4 samples per period
 
 %% signal
-freq = 1000;    % signal frequency
+freq = 5000;    % signal frequency
 phi0 = pi/4;    % initial signal phase
-ampl = 1;       % signal amplitude
+ampl = 5;       % signal amplitude
 T = 0.15;       % signal duration
 
 %% noise
-noiseVariance = 0.5;
+noiseVariance = 10;
 
 %% delay & attenuation
 atten = 0.3;
